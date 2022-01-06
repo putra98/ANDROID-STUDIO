15f1016dbc6bad4fac1810f1954b221bbae04562
Moh Saputra Pangestu <53217950+putra98@users.noreply.github.com>
2022-01-06 08:55:10 +0700

    Add files via upload

---
+++ FragmenApp/app/src/main/java/com/example/fragmenapp/FirstFragment.java
@@ -0,0 +1,6 @@
+package com.example.fragmenapp;
+
+import android.app.Fragment;
+
+public class FirstFragment extends Fragment {
+}
---
+++ FragmenApp/app/src/main/java/com/example/fragmenapp/SecondActivity.java
@@ -0,0 +1,6 @@
+package com.example.fragmenapp;
+
+import android.app.Activity;
+
+public class SecondActivity extends Activity {
+}
